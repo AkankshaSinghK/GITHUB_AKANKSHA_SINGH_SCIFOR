088cae3c7dd21fbbab227541374aa1913e07b26f
AkankshaSinghK <singhakanksha2897@gmail.com>
2024-06-11 17:35:22 +0530

    assignment 6 completed

---
+++ Assignment_6/script.js
@@ -0,0 +1,14 @@
+ // Variables for favorite things
+ var favoriteFood = "Pizza";
+ var favoriteColor = "Blue";
+ var favoriteVehicle = "Car";
+
+ // Using document.write() to print variables
+ document.write("Favorite Food: " + favoriteFood + "<br>");
+ document.write("Favorite Color: " + favoriteColor + "<br>");
+ document.write("Favorite Vehicle: " + favoriteVehicle + "<br>");
+
+ // Using document.writeln() to print variables
+ document.writeln("Favorite Food: " + favoriteFood);
+ document.writeln("Favorite Color: " + favoriteColor);
+ document.writeln("Favorite Vehicle: " + favoriteVehicle);
